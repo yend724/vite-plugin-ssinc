fdf7e15d4e62cea140f210ba0f5782db69fee7f9
yend724 <yend9524@gmail.com>
2023-04-25 13:14:10 +0900

    Add the  option and update the README.md accordingly

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,12 @@
 import { DEFAULT_OPTIONS } from './options.js';
 import type { Options } from './index.js';
 
-export const formatOptions = (_options: Options): Required<Options> => {
+export const formatOptions = (_options: Options) => {
   const includeExtensions =
     _options?.includeExtensions ?? DEFAULT_OPTIONS['includeExtensions'];
 
   const options = {
+    ..._options,
     includeExtensions: includeExtensions.map(ex => '.' + ex),
   };
 
